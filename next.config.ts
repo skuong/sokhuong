2c1bbbde9c17cef5acac9f0205f4e0b626d1e1a8
Sokhuong <sokhuong.usk@gmail.com>
2026-07-01 06:33:37 +0700

    feat: add timeline placeholder

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,13 @@
 import type { NextConfig } from "next"
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'images.unsplash.com',
+      }
+    ]
+  }
+}
 
 export default nextConfig
