5123e33c9c1532f4d65ffa7eaf9f98fee9737017
Sokhuong <sokhuong.usk@gmail.com>
2026-07-01 12:27:04 +0700

    feat: add work experience timeline

---
+++ features/work/components/work-experience-card.tsx
@@ -0,0 +1,90 @@
+import { differenceInMonths } from "date-fns/fp"
+import { Plus } from "lucide-react"
+
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarGroup,
+  AvatarGroupCount,
+  AvatarImage
+} from "@/components/ui/avatar"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from "@/components/ui/card"
+
+export type WorkExperience = {
+  id: number
+  company: string
+  role: string
+  startDate: string
+  endDate: string
+  previewWorkEndDate: string
+}
+
+export function WorkExperienceCard({ work }: { work: WorkExperience }) {
+  const previewWorkEndDate = new Date(work.previewWorkEndDate)
+
+  const startDate = new Date(work.startDate)
+  const endDate = new Date(work.endDate)
+  const startPosition = differenceInMonths(previewWorkEndDate, startDate)
+
+  const width = differenceInMonths(startDate, endDate)
+
+  return (
+    <Card
+      className="relative h-64 shrink-0 overflow-visible"
+      style={{
+        width: `${width * 4}rem`,
+        marginLeft: `${startPosition * 3}rem`
+      }}
+    >
+      <CardHeader>
+        <CardTitle className="text-4xl">{work.company}</CardTitle>
+        <CardDescription className="text-xl">{work.role}</CardDescription>
+        <CardAction>
+          <Plus className="size-6 text-muted" />
+        </CardAction>
+      </CardHeader>
+
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+
+      <CardFooter className="mt-auto">
+        <AvatarGroup className="ml-auto grayscale">
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          <Avatar>
+            <AvatarImage
+              src="https://github.com/maxleiter.png"
+              alt="@maxleiter"
+            />
+            <AvatarFallback>LR</AvatarFallback>
+          </Avatar>
+          <Avatar>
+            <AvatarImage
+              src="https://github.com/evilrabbit.png"
+              alt="@evilrabbit"
+            />
+            <AvatarFallback>ER</AvatarFallback>
+          </Avatar>
+          <AvatarGroupCount>+3</AvatarGroupCount>
+        </AvatarGroup>
+      </CardFooter>
+
+      <div className="absolute -bottom-24 h-20 w-px bg-muted"></div>
+
+      <div className="absolute -bottom-36 text-3xl text-muted-foreground">
+        {new Date(work.startDate).getFullYear()}
+      </div>
+    </Card>
+  )
+}
--- features/work/components/work-timeline-scroll-area.tsx
+++ features/work/components/work-timeline-scroll-area.tsx
@@ -1,49 +1,58 @@
-import Image from "next/image"
-
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
+import {
+  WorkExperience,
+  WorkExperienceCard
+} from "@/features/work/components/work-experience-card"
 
-type Artwork = {
-  artist: string
-  art: string
-}
+const works: WorkExperience[] = [
+  {
+    id: 1,
+    company: "Zeal",
+    role: "Research Intern",
+    startDate: "2019-06-01",
+    endDate: "2020-03-30",
+    previewWorkEndDate: "2019-06-01"
+  },
+  {
+    id: 2,
+    company: "Quadusk",
+    role: "Co-founder, Developer Lead",
+    startDate: "2020-05-01",
+    endDate: "2022-09-30",
+    previewWorkEndDate: "2020-03-30"
+  },
+  {
+    id: 3,
+    company: "Kirirom Digital Cambodia",
+    role: "Web Developer",
+    startDate: "2023-02-01",
+    endDate: "2024-01-31",
+    previewWorkEndDate: "2022-09-30"
+  },
+  {
+    id: 4,
+    company: "StringBoard4Education",
+    role: "Web & System Developer",
+    startDate: "2024-02-15",
+    endDate: "2025-03-31",
+    previewWorkEndDate: "2024-01-31"
+  },
+  {
+    id: 5,
+    company: "TUX Global Institute",
+    role: "Web Developer",
+    startDate: "2025-01-04",
+    endDate: "2026-05-29",
+    previewWorkEndDate: "2025-01-01"
+  }
+]
 
 export function WorkTimelineScrollArea() {
-  const works: Artwork[] = [
-    {
-      artist: "Ornella Binni",
-      art: "https://images.unsplash.com/photo-1465869185982-5a1a7522cbcb?auto=format&fit=crop&w=300&q=80"
-    },
-    {
-      artist: "Tom Byrom",
-      art: "https://images.unsplash.com/photo-1548516173-3cabfa4607e9?auto=format&fit=crop&w=300&q=80"
-    },
-    {
-      artist: "Vladimir Malyavko",
-      art: "https://images.unsplash.com/photo-1494337480532-3725c85fd2ab?auto=format&fit=crop&w=300&q=80"
-    }
-  ]
-
   return (
-    <ScrollArea className="rounded-md border whitespace-nowrap">
-      <div className="flex space-x-4 p-4">
-        {works.map((artwork) => (
-          <figure key={artwork.artist} className="shrink-0">
-            <div className="overflow-hidden rounded-md">
-              <Image
-                src={artwork.art}
-                alt={`Photo by ${artwork.artist}`}
-                className="aspect-3/4 h-fit object-cover"
-                width={300}
-                height={400}
-              />
-            </div>
-            <figcaption className="pt-2 text-xs text-muted-foreground">
-              Photo by{" "}
-              <span className="font-semibold text-foreground">
-                {artwork.artist}
-              </span>
-            </figcaption>
-          </figure>
+    <ScrollArea className="">
+      <div className="flex w-fit gap-4 px-16 pt-24 pb-36">
+        {works.map((work) => (
+          <WorkExperienceCard work={work} key={work.id} />
         ))}
       </div>
 
