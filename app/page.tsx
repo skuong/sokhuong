da86a04bbd0d41785d0e8d0a4e95e5bb3484f852
Sokhuong <sokhuong.usk@gmail.com>
2026-07-01 17:07:17 +0700

    feat: add placeholder for showcasing nice UI

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,22 @@ export default function HomePage() {
   return (
     <main>
       <WorkTimelineScrollArea />
+      <div className="space-y-8 py-56 md:space-y-16">
+        <div className="text-center text-6xl font-bold uppercase md:text-8xl lg:text-9xl">
+          Enhance
+        </div>
+
+        <div className="text-center text-6xl font-bold uppercase md:text-8xl lg:text-[10rem] xl:text-[12rem]">
+          Easthetics
+        </div>
+
+        <div className="py-24">
+          <div className="grid grid-cols-8 gap-8">
+            <div className="col-span-5 h-165 rounded-md bg-muted/50"></div>
+            <div className="col-span-3 h-165 rounded-md bg-muted/50"></div>
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
