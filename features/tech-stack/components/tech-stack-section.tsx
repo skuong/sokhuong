287f10b061f252adf6d49e38265a71ed83873daf
Sokhuong <sokhuong.usk@gmail.com>
2026-07-02 17:50:19 +0700

    feat: added a bunch of logos in tech stack section

--- features/tech-stack/components/tech-stack-section.tsx
+++ features/tech-stack/components/tech-stack-section.tsx
@@ -4,6 +4,16 @@ import { useGSAP } from "@gsap/react"
 
 import { gsap } from "@/lib/gsap"
 
+import { GolangLogo } from "./golang-logo"
+import { GsapLogo } from "./gsap-logo"
+import { MotionLogo } from "./motion-logo"
+import { ReactJsLogo } from "./react-js-logo"
+import { RustLogo } from "./rust-logo"
+import { TailwindCssLogo } from "./tailwindcss-logo"
+import { ThreeJsLogo } from "./three-js-logo"
+import { TypeScriptLogo } from "./type-script-logo"
+import { VueJsLogo } from "./vue-js-logo"
+
 export function TechStackSection() {
   const header1 = useRef(null)
   const header2 = useRef(null)
@@ -48,9 +58,36 @@ export function TechStackSection() {
       </h2>
 
       <div className="mt-36 grid grid-cols-6 px-24">
-        <div className="col-span-2 aspect-square border bg-muted">A</div>
-        <div className="col-span-2 aspect-square border bg-muted">A</div>
-        <div className="col-span-2 aspect-square border bg-muted">A</div>
+        <div className="col-span-2 flex aspect-square items-center justify-center p-8">
+          <ReactJsLogo />
+        </div>
+        <div className="col-span-2 aspect-square border text-black/30">
+          <TypeScriptLogo />
+        </div>
+        <div className="col-span-2 aspect-square">
+          <ThreeJsLogo />
+        </div>
+
+        <div className="col-span-1 flex aspect-square items-center justify-center p-4">
+          <TailwindCssLogo />
+        </div>
+        <div className="col-span-1 flex aspect-square items-center justify-center p-4">
+          <VueJsLogo />
+        </div>
+        <div className="col-span-1 flex aspect-square items-center justify-center p-4">
+          <RustLogo />
+        </div>
+
+        <div className="col-span-1 flex aspect-square items-center justify-center p-4">
+          <GolangLogo />
+        </div>
+
+        <div className="col-span-1 flex aspect-square items-center justify-center p-4">
+          <GsapLogo />
+        </div>
+        <div className="col-span-1 flex aspect-square items-center justify-center p-4">
+          <MotionLogo />
+        </div>
 
         <div className="col-span-1 aspect-square border bg-muted">A</div>
         <div className="col-span-1 aspect-square border bg-muted">A</div>
