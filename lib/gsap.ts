de1d7fd8178f710289e509ba065fae4d728ef528
Sokhuong <sokhuong.usk@gmail.com>
2026-07-02 13:12:50 +0700

    feat: add scroll animation for interactive application section

--- lib/gsap.ts
+++ lib/gsap.ts
@@ -1,6 +1,10 @@
 import { gsap } from "gsap"
+import { GSDevTools } from "gsap/GSDevTools"
 import { ScrollToPlugin } from "gsap/ScrollToPlugin"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
 
+gsap.registerPlugin(GSDevTools)
 gsap.registerPlugin(ScrollToPlugin)
+gsap.registerPlugin(ScrollTrigger)
 
 export { gsap }
