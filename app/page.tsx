7c735f98e792fd5a1b944a3aacd47cd7f2d90f95
Sokhuong <sokhuong.usk@gmail.com>
2026-07-02 10:52:10 +0700

    chore: hide aesthetic section

--- app/page.tsx
+++ app/page.tsx
@@ -32,24 +32,6 @@ export default function HomePage() {
           <WorkTimelineScrollArea />
         </div>
 
-        <div className="space-y-8 py-56 md:space-y-16">
-          <div className="text-center text-6xl font-bold uppercase md:text-8xl lg:text-9xl">
-            Enhance
-          </div>
-
-          <div className="text-center text-6xl font-bold uppercase md:text-8xl lg:text-[10rem]">
-            Easthetics
-          </div>
-
-          <div className="py-24">
-            <div className="grid grid-cols-8 gap-8">
-              <div className="col-span-5 h-210 rounded-md bg-muted/50"></div>
-              <div className="col-span-3 h-210 rounded-md bg-muted/50"></div>
-              <div className="col-span-8 h-screen rounded-md bg-muted/50"></div>
-            </div>
-          </div>
-        </div>
-
         <InteractiveApplicationsSection />
       </main>
     </div>
