23102ab80236f7126971999536dfad62aea0b972
Sokhuong <sokhuong.usk@gmail.com>
2026-07-02 13:59:39 +0700

    chore: use gsap react

--- features/art/components/phonetic-name-with-hover-card.tsx
+++ features/art/components/phonetic-name-with-hover-card.tsx
@@ -1,24 +1,26 @@
+"use client"
+
 import {
   HoverCard,
   HoverCardContent,
   HoverCardTrigger
 } from "@/components/ui/hover-card"
-import { NameInPhoneticSymbolsSvg } from "@/features/art/components/name-in-phonetic-symbols-svg"
 
 export function PhoneticNameWithHoverCard() {
   return (
     <HoverCard>
       <HoverCardTrigger
         delay={200}
-        className="cursor-pointer"
-        render={<NameInPhoneticSymbolsSvg />}
-      />
+        className="cursor-pointer font-noto-sans text-9xl font-bold"
+      >
+        <span id="name-phonetic-symbols">kʊəŋ</span>
+      </HoverCardTrigger>
       <HoverCardContent className="w-fit">
         <div className="flex flex-col">
           <div>I&apos;m Sokhuong Uon.</div>
           <div>
-            You can call me Khuong{" "}
-            <span className="font-mono text-lg">/kʊəŋ/</span>
+            You can call me Khuong /
+            <span className="font-noto-sans text-lg">kʊəŋ</span>/
           </div>
         </div>
       </HoverCardContent>
