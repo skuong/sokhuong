8830b1f038a7aa8c962c65d0f109609d4adbd98d
Sokhuong <sokhuong.usk@gmail.com>
2026-07-03 13:47:42 +0700

    fix: fix z index for work experience section

--- app/page.tsx
+++ app/page.tsx
@@ -44,7 +44,7 @@ export default function HomePage() {
       </header>
 
       <main>
-        <div className="relative h-[calc(100vh-10rem)]">
+        <div className="relative z-20 h-[calc(100vh-10rem)]">
           <WorkTimelineScrollArea />
         </div>
 
