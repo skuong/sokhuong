80e6b30f9b847ea1df3b7043282cb668f9848405
Sokhuong <sokhuong.usk@gmail.com>
2026-07-03 18:08:49 +0700

    feat: add persistance mouse trails effect

--- features/tech-stack/components/colored-tech-stack-logo-grid.tsx
+++ features/tech-stack/components/colored-tech-stack-logo-grid.tsx
@@ -12,16 +12,16 @@ import { ThreeJsLogo } from "./three-js-logo"
 import { TypeScriptLogo } from "./type-script-logo"
 import { VueJsLogo } from "./vue-js-logo"
 
-export function TechStackLogoGrid({
+export function ColoredTechStackLogoGrid({
   className,
   ...props
 }: ComponentProps<"div">) {
   return (
     <div className={cn("grid grid-cols-6", className)} {...props}>
-      <div className="col-span-2 flex aspect-square items-center justify-center p-8">
+      <div className="col-span-2 flex aspect-square items-center justify-center p-8 text-blue-800">
         <ReactJsLogo />
       </div>
-      <div className="col-span-2 aspect-square">
+      <div className="col-span-2 aspect-square text-black/30">
         <TypeScriptLogo />
       </div>
       <div className="col-span-2 aspect-square">
@@ -32,22 +32,7 @@ export function TechStackLogoGrid({
         <TailwindCssLogo />
       </div>
       <div className="col-span-1 flex aspect-square items-center justify-center p-4">
-        <svg viewBox="0 0 261.76 226.69">
-          <g transform="matrix(1.3333 0 0 -1.3333 -76.311 313.34)">
-            <g transform="translate(178.06 235.01)">
-              <path
-                d="m0 0-22.669-39.264-22.669 39.264h-75.491l98.16-170.02 98.16 170.02z"
-                fill="currentColor"
-              />
-            </g>
-            <g transform="translate(178.06 235.01)">
-              <path
-                d="m0 0-22.669-39.264-22.669 39.264h-36.227l58.896-102.01 58.896 102.01z"
-                fill="currentColor"
-              />
-            </g>
-          </g>
-        </svg>
+        <VueJsLogo />
       </div>
       <div className="col-span-1 flex aspect-square items-center justify-center p-4">
         <RustLogo />
