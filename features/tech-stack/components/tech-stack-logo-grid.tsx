31619d98cfd0c5a410ad387c83a428de78492cce
Sokhuong <sokhuong.usk@gmail.com>
2026-07-04 19:15:26 +0700

    feat: added blender and figma logo

--- features/tech-stack/components/tech-stack-logo-grid.tsx
+++ features/tech-stack/components/tech-stack-logo-grid.tsx
@@ -2,6 +2,8 @@ import { ComponentProps } from "react"
 
 import { cn } from "@/lib/utils"
 
+import { NoColorBlenderLogo } from "./no-color-logo/blender-logo"
+import { NoColorFigmaLogo } from "./no-color-logo/figma-logo"
 import { GolangLogo } from "./no-color-logo/golang-logo"
 import { GsapLogo } from "./no-color-logo/gsap-logo"
 import { MotionLogo } from "./no-color-logo/motion-logo"
@@ -10,7 +12,7 @@ import { RustLogo } from "./no-color-logo/rust-logo"
 import { TailwindCssLogo } from "./no-color-logo/tailwindcss-logo"
 import { ThreeJsLogo } from "./no-color-logo/three-js-logo"
 import { TypeScriptLogo } from "./no-color-logo/type-script-logo"
-import { VueJsLogo } from "./no-color-logo/vue-js-logo"
+import { NoColorVueJsLogo, VueJsLogo } from "./no-color-logo/vue-js-logo"
 
 export function TechStackLogoGrid({
   className,
@@ -32,22 +34,7 @@ export function TechStackLogoGrid({
         <TailwindCssLogo />
       </div>
       <div className="col-span-1 flex aspect-square items-center justify-center p-4">
-        <svg viewBox="0 0 261.76 226.69">
-          <g transform="matrix(1.3333 0 0 -1.3333 -76.311 313.34)">
-            <g transform="translate(178.06 235.01)">
-              <path
-                d="m0 0-22.669-39.264-22.669 39.264h-75.491l98.16-170.02 98.16 170.02z"
-                fill="currentColor"
-              />
-            </g>
-            <g transform="translate(178.06 235.01)">
-              <path
-                d="m0 0-22.669-39.264-22.669 39.264h-36.227l58.896-102.01 58.896 102.01z"
-                fill="#888"
-              />
-            </g>
-          </g>
-        </svg>
+        <NoColorVueJsLogo />
       </div>
       <div className="col-span-1 flex aspect-square items-center justify-center p-4">
         <RustLogo />
@@ -68,8 +55,12 @@ export function TechStackLogoGrid({
       <div className="col-span-1 aspect-square border bg-muted">A</div>
       <div className="col-span-1 aspect-square border bg-muted">A</div>
       <div className="col-span-1 aspect-square border bg-muted">A</div>
-      <div className="col-span-1 aspect-square border bg-muted">A</div>
-      <div className="col-span-1 aspect-square border bg-muted">A</div>
+      <div className="col-span-1 flex aspect-square items-center justify-center p-4">
+        <NoColorBlenderLogo />
+      </div>
+      <div className="col-span-1 flex aspect-square items-center justify-center p-4">
+        <NoColorFigmaLogo />
+      </div>
     </div>
   )
 }
