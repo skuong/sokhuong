debd59e2a2c969d122949c47fce4c1ccd610a5ff
Sokhuong <sokhuong.usk@gmail.com>
2026-07-04 14:29:35 +0700

    chore: make cta bigger

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,9 @@ import { Canvas } from "@react-three/fiber"
 import { GSDevTools } from "gsap/GSDevTools"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
 import Lenis from "lenis"
+import { Plus } from "lucide-react"
 
+import { Cursor } from "@/components/ui/custom-cursor"
 import { PhoneticNameWithHoverCard } from "@/features/art/components/phonetic-name-with-hover-card"
 import { HeroSection } from "@/features/hero/components/hero-section"
 import { InteractiveApplicationsSection } from "@/features/interactive-application/components/interactive-applications-section"
@@ -31,7 +33,7 @@ export default function HomePage() {
   }, [])
 
   return (
-    <div ref={canvasContainer}>
+    <div ref={canvasContainer} className="relative border">
       <div className="fixed inset-0 z-10">
         {canvasContainer && (
           // @ts-expect-error the canvasContainer will be available
@@ -39,6 +41,24 @@ export default function HomePage() {
             <View.Port />
           </Canvas>
         )}
+      </div>
+
+      <div className="group absolute right-0 z-50 flex h-[calc(100vh-18rem)] w-4/12 bg-[repeating-linear-gradient(-45deg,var(--color-neutral-300)_0,var(--color-neutral-300)_1px,transparent_1px,transparent_10px)] dark:bg-[repeating-linear-gradient(-45deg,var(--color-neutral-800)_0,var(--color-neutral-800)_1px,transparent_1px,transparent_10px)]">
+        <Cursor
+          name="Work together"
+          className="m-auto flex"
+          cursorColor="white"
+          svgClassName="size-14"
+        >
+          <div className="group z-10 flex size-56 bg-rose-800">
+            <Plus
+              strokeLinecap="round"
+              className="m-auto size-24 rounded-full stroke-[0.5px] text-rose-300"
+            />
+          </div>
+        </Cursor>
+
+        <div className="absolute top-1/2 left-1/2 size-56 -translate-x-1/2 -translate-y-1/2 border border-r border-b border-rose-800 bg-black transition-transform duration-200 group-hover:-translate-x-5/12 group-hover:-translate-y-5/12"></div>
       </div>
 
       <header className="container mx-auto pt-16">
