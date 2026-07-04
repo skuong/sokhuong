2e3fd5884d6ecbea095244d1a79f250844f83a64
Sokhuong <sokhuong.usk@gmail.com>
2026-07-04 16:43:40 +0700

    feat: animated interactive application title

--- features/interactive-application/components/interactive-applications-section.tsx
+++ features/interactive-application/components/interactive-applications-section.tsx
@@ -13,43 +13,44 @@ const InteractiveApplicationsSection = () => {
   const { resolvedTheme } = useTheme()
   const titleLine1Ref = useRef(null)
   const titleLine2Ref = useRef(null)
+  const sectionRef = useRef(null)
 
   useGSAP(() => {
-    gsap.from(titleLine1Ref.current, {
+    const tl = gsap.timeline({
       scrollTrigger: {
-        trigger: titleLine1Ref.current,
-        start: "top bottom",
-        end: "bottom 40%",
-        scrub: true
-      },
-      x: 300
+        trigger: sectionRef.current,
+        start: "center center",
+        end: "+=5000",
+        scrub: true,
+        pin: true,
+        pinSpacing: true
+        // markers: true
+      }
     })
 
-    gsap.from(titleLine2Ref.current, {
-      scrollTrigger: {
-        trigger: titleLine2Ref.current,
-        start: "top bottom",
-        end: "bottom 40%",
-        scrub: true
-      },
-      x: -300
-    })
+    tl.from(titleLine1Ref.current, { scale: 20, y: 1300, x: -50 }).from(
+      titleLine2Ref.current,
+      { scale: 20, y: 4000 },
+      0
+    )
   }, [])
 
   return (
     <section className="mt-36">
-      <div
-        ref={titleLine1Ref}
-        className="text-center text-6xl font-bold uppercase md:text-8xl lg:text-[9rem] xl:text-[10rem]"
-      >
-        Interactive
-      </div>
+      <div ref={sectionRef}>
+        <div
+          ref={titleLine1Ref}
+          className="text-center text-6xl font-bold uppercase md:text-8xl lg:text-[9rem] xl:text-[10rem]"
+        >
+          Interactive
+        </div>
 
-      <div
-        ref={titleLine2Ref}
-        className="text-center text-6xl font-bold uppercase md:text-8xl lg:text-[9rem] xl:text-[10rem]"
-      >
-        Applications
+        <div
+          ref={titleLine2Ref}
+          className="text-center text-6xl font-bold uppercase md:text-8xl lg:text-[9rem] xl:text-[10rem]"
+        >
+          Applications
+        </div>
       </div>
 
       <div className="py-24">
