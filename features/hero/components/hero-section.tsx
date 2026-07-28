43e7a32596f57f5004373c8b99984d2b2c9d72d9
Sokhuong <sokhuong.usk@gmail.com>
2026-07-28 16:08:27 +0700

    feat: Added placeholder video for hero section

--- features/hero/components/hero-section.tsx
+++ features/hero/components/hero-section.tsx
@@ -16,30 +16,58 @@ gsap.registerPlugin(SplitText)
 export function HeroSection() {
   const softwareTitleRef = useRef<HTMLDivElement>(null)
   const engineerTitleRef = useRef<HTMLDivElement>(null)
+  const videoRef = useRef<HTMLVideoElement>(null)
 
   useGSAP(() => {
     gsap.set([softwareTitleRef.current, engineerTitleRef.current], {
       autoAlpha: 1
     })
 
-    gsap.to(softwareTitleRef.current, {
-      duration: 1,
-      scrambleText: {
-        text: "CREATIVE",
-        chars: "C%#/$",
-        revealDelay: 0.5,
-        speed: 0.5
-      }
-    })
-    gsap.to(engineerTitleRef.current, {
-      duration: 1,
-      scrambleText: {
-        text: "DEVELOPER",
-        chars: "/@#!*",
-        revealDelay: 0.5,
-        speed: 0.3,
-        newClass: "myClass"
-      }
+    const tl = gsap.timeline()
+    tl.add(() => {
+      const split = new SplitText(softwareTitleRef.current, { type: "chars" })
+      const iChar = split.chars[5]
+
+      if (!iChar || !videoRef.current) return
+
+      gsap.set(iChar, {
+        position: "relative",
+        yPercent: 15,
+        display: "inline-block",
+        overflow: "hidden"
+      })
+
+      iChar.appendChild(videoRef.current)
+      gsap.set(videoRef.current, {
+        position: "absolute",
+        top: 0,
+        left: "50%",
+        xPercent: -49,
+        yPercent: 20,
+        height: "72%",
+        objectFit: "cover",
+        autoAlpha: 0
+      })
+
+      gsap
+        .timeline()
+        .to(iChar, {
+          width: () => (window.innerWidth >= 1024 ? 170 : 150),
+          duration: 1,
+          ease: "power3.inOut",
+          delay: 0.5
+        })
+        .to(
+          videoRef.current,
+          {
+            autoAlpha: 1,
+            width: () => (window.innerWidth >= 1024 ? 160 : 150),
+            transformOrigin: "left center",
+            duration: 0.3
+          },
+          "<"
+        )
+        .call(() => videoRef.current?.play())
     })
   }, [])
 
@@ -49,7 +77,7 @@ export function HeroSection() {
         <div className="font-mono font-light text-muted-foreground">KHUONG</div>
         <div className="font-mono font-light text-muted-foreground">MENU</div>
       </div>
-      <div className="flex flex-col items-center text-6xl font-bold md:text-9xl">
+      <div className="mx-auto flex flex-col items-start justify-start text-6xl font-bold md:text-9xl lg:text-[9rem]">
         <div ref={softwareTitleRef} className="opacity-0">
           CREATIVE
         </div>
@@ -57,6 +85,16 @@ export function HeroSection() {
           DEVELOPER
         </div>
       </div>
+
+      <video
+        ref={videoRef}
+        src="https://placeholdervideo.dev/1280x720"
+        muted
+        loop
+        playsInline
+        className="absolute h-0 bg-white object-cover opacity-0"
+      />
+
       <div className="flex items-end justify-between">
         <div>
           <div className="relative size-24 bg-muted md:size-32">
