986a86ec8a2f7b45231e38b4627c155476f552f1
Sokhuong <sokhuong.usk@gmail.com>
2026-08-04 15:09:47 +0700

    feat: ADd intro animation

--- features/intro/components/intro-section.tsx
+++ features/intro/components/intro-section.tsx
@@ -1,33 +1,67 @@
 import { useRef } from "react"
 
 import { useGSAP } from "@gsap/react"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+import { SplitText } from "gsap/SplitText"
 
 import { gsap } from "@/lib/gsap"
 
-gsap.registerPlugin(useGSAP)
+gsap.registerPlugin(useGSAP, SplitText, ScrollTrigger)
 
 export function IntroSection() {
   const introductionSection = useRef<HTMLDivElement>(null)
+  const intro = useRef<HTMLDivElement>(null)
 
   useGSAP(
     () => {
-      gsap.fromTo(
-        "#introduction",
+      const getScrollAmount = () => {
+        return (
+          introductionSection.current!.offsetWidth -
+          window.innerWidth +
+          window.innerWidth / 2
+        )
+      }
+
+      console.log(intro.current?.offsetWidth)
+      const tween = gsap.fromTo(
+        intro.current,
         {
-          xPercent: 110
+          x: window.innerWidth
         },
         {
-          xPercent: -240,
-
-          scrollTrigger: {
-            trigger: introductionSection.current,
-            start: "top 45%",
-            end: "+=2000",
-            scrub: true,
-            pin: true
-          }
+          x: () => -getScrollAmount(),
+          duration: 3,
+          ease: "none"
         }
       )
+
+      ScrollTrigger.create({
+        trigger: introductionSection.current,
+        start: "top 50%",
+        end: () => `+=${getScrollAmount()}`,
+        scrub: true,
+        pin: true,
+        markers: true,
+        animation: tween,
+        invalidateOnRefresh: true
+      })
+
+      intro.current?.querySelectorAll(":scope > div").forEach((node, index) => {
+        const contentElement = node.firstElementChild
+
+        ScrollTrigger.create({
+          trigger: node,
+          containerAnimation: tween,
+          start: "left 80%",
+          end: "right 50%",
+          animation: gsap.from(contentElement, {
+            y: () => (index % 2 === 0 ? -100 : 100),
+            opacity: 0
+          }),
+          scrub: true,
+          markers: true
+        })
+      })
     },
     {
       scope: introductionSection
@@ -35,12 +69,39 @@ export function IntroSection() {
   )
 
   return (
-    <section ref={introductionSection} className="text-9xl font-bold">
-      <p id="introduction" className="whitespace-nowrap">
-        <span id="greeting">Hi I&apos;m Sokhuong</span>
-        <span>, </span>
-        <span id="what-i-do">creative developer based in Phnom Penh.</span>
-      </p>
-    </section>
+    <div ref={introductionSection} className="w-max text-9xl font-bold">
+      <div ref={intro} id="introduction" className="flex gap-6">
+        <div>
+          <div>Hi</div>
+        </div>
+        <div>
+          <div>I&apos;m</div>
+        </div>
+        <div>
+          <div>Sokhuong,</div>
+        </div>
+        <div className="relative mr-128">
+          <div>creative</div>
+          <div className="absolute -top-4 -right-4 translate-x-full -translate-y-1/2 -rotate-z-12 rounded-lg bg-lime-400 px-6 py-4 text-black">
+            Web
+          </div>
+          <div className="absolute -right-4 -bottom-4 translate-x-196 translate-y-full rounded-lg bg-indigo-300 px-6 py-4 text-black">
+            Desktop
+          </div>
+        </div>
+        <div>
+          <div>developer</div>
+        </div>
+        <div>
+          <div>based</div>
+        </div>
+        <div>
+          <div>in</div>
+        </div>
+        <div>
+          <div>Phnom Penh.</div>
+        </div>
+      </div>
+    </div>
   )
 }
