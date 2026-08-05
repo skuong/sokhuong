b2c54230057897d5e429d6b121d530432fef1af6
Sokhuong <sokhuong.usk@gmail.com>
2026-08-05 18:02:50 +0700

    feat: Make book call modal responsive

--- app/@contact/(.)book-a-call/page.tsx
+++ app/@contact/(.)book-a-call/page.tsx
@@ -2,35 +2,83 @@
 
 import { useRouter } from "next/navigation"
 
+import { Button } from "@/components/ui/button"
 import {
   Dialog,
   DialogContent,
   DialogHeader,
   DialogTitle
 } from "@/components/ui/dialog"
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger
+} from "@/components/ui/drawer"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { BookACall } from "@/features/contact/components/book-a-call"
+import { useMediaQuery } from "@/hooks/use-media-query"
+import { cn } from "@/lib/utils"
 
 export default function BookACallPage() {
   const router = useRouter()
+  const isDesktop = useMediaQuery("(min-width: 768px)")
+
+  if (isDesktop) {
+    return (
+      <Dialog
+        open
+        onOpenChange={(open) => {
+          if (!open) router.back()
+        }}
+      >
+        <DialogContent
+          className={
+            "overflow-hidden pt-12 md:max-w-[90vw] lg:h-[90dvh] lg:max-h-[90dvh]"
+          }
+        >
+          <DialogHeader className="sr-only">
+            <DialogTitle>Let's talk</DialogTitle>
+          </DialogHeader>
+
+          <Tabs defaultValue="15min" className="overflow-hidden">
+            <div className="flex w-full justify-center px-8">
+              <TabsList className={"md:w-1/2"}>
+                <TabsTrigger value="15min">15 minutes</TabsTrigger>
+                <TabsTrigger value="30min">30 minutes</TabsTrigger>
+              </TabsList>
+            </div>
+
+            <TabsContent keepMounted value="15min" className={"w-full"}>
+              <BookACall eventSlug="15min" />
+            </TabsContent>
+            <TabsContent keepMounted value="30min">
+              <BookACall eventSlug="30min" />
+            </TabsContent>
+          </Tabs>
+        </DialogContent>
+      </Dialog>
+    )
+  }
 
   return (
-    <Dialog
+    <Drawer
       open
       onOpenChange={(open) => {
         if (!open) router.back()
       }}
+      showSwipeHandle
     >
-      <DialogContent
-        className={
-          "overflow-hidden pt-12 md:h-[80dvh] md:max-h-[90dvh] md:max-w-[80vw]"
-        }
-      >
-        <DialogHeader className="sr-only">
-          <DialogTitle>Let's talk</DialogTitle>
-        </DialogHeader>
+      <DrawerContent className={"h-dvh"}>
+        <DrawerHeader className="sr-only">
+          <DrawerTitle>Book a call</DrawerTitle>
+        </DrawerHeader>
 
-        <Tabs defaultValue="15min" className="overflow-hidden">
+        <Tabs defaultValue="15min" className="overflow-hidden pt-4">
           <div className="flex w-full justify-center px-8">
             <TabsList className={"md:w-1/2"}>
               <TabsTrigger value="15min">15 minutes</TabsTrigger>
@@ -45,7 +93,7 @@ export default function BookACallPage() {
             <BookACall eventSlug="30min" />
           </TabsContent>
         </Tabs>
-      </DialogContent>
-    </Dialog>
+      </DrawerContent>
+    </Drawer>
   )
 }
