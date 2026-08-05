5a956a0c8524b5dfd250b87046e58a4bdcc970b3
Sokhuong <sokhuong.usk@gmail.com>
2026-08-05 17:32:38 +0700

    chore: Removed a link for skipping to main content

--- app/page.tsx
+++ app/page.tsx
@@ -41,13 +41,6 @@ export default function HomePage() {
         )}
       </div>
 
-      <Link
-        href={"#main"}
-        className="pointer-events-none absolute top-4 left-4 bg-background p-2 opacity-0 ring-0 outline-none focus-visible:opacity-100 focus-visible:ring-2 focus-visible:ring-pink-600"
-      >
-        Skip to main content
-      </Link>
-
       <Header />
 
       <main id="main">
