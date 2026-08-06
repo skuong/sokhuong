7ed0557655ac32196f69385a4d4047a6b6a226e4
Sokhuong <sokhuong.usk@gmail.com>
2026-08-06 16:57:05 +0700

    feat: Add link to home from sketch page

--- features/sketches/components/horizontal-carousel-icon.tsx
+++ features/sketches/components/horizontal-carousel-icon.tsx
@@ -1,11 +1,10 @@
 export function HorizontalCarouselIcon() {
   return (
     <svg
-      width="24"
-      height="24"
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      className="h-4 w-4"
     >
       <title>Horizonal carousel</title>
 
