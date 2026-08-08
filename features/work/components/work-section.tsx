1437ac7ffe47326ed3ba21629e25a7bc0ded8265
Sokhuong <sokhuong.usk@gmail.com>
2026-08-08 11:58:11 +0700

    feat: Add clip path animation for second image

--- features/work/components/work-section.tsx
+++ features/work/components/work-section.tsx
@@ -17,6 +17,9 @@ export function WorkSection() {
       gsap.set("#project-description h3", {
         clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 100%)"
       })
+      gsap.set("#work-lighlight-figure-2", {
+        clipPath: "polygon(0% 110%, 100% 100%, 100% 100%, 0% 110%)"
+      })
 
       const projectDescriptionParagraphOuterSplit = new SplitText(
         "#project-description-paragraph",
@@ -34,9 +37,10 @@ export function WorkSection() {
       const introTl = gsap.timeline({
         scrollTrigger: {
           trigger: workSection.current,
-          start: "top 90%",
-          end: "top top",
-          scrub: true
+          start: "top 80%",
+          end: "bottom top",
+          scrub: true,
+          markers: true
         }
       })
 
@@ -57,11 +61,19 @@ export function WorkSection() {
           },
           "<0.2"
         )
+        .to(["#work-intro-section-title"], {
+          opacity: 0,
+          duration: 1
+        })
+        .to(["#work-intro-section-of-my"], {
+          opacity: 0,
+          duration: 1
+        })
 
       const tl = gsap.timeline({
         scrollTrigger: {
           trigger: workSection.current,
-          markers: true,
+          // markers: true,
           scrub: true,
           pin: true
         }
@@ -110,6 +122,9 @@ export function WorkSection() {
         },
         "<"
       )
+      tl.to("#work-lighlight-figure-2", {
+        clipPath: "polygon(0% 0%, 100% -10%, 100% 100%, 0% 100%)"
+      })
     },
     {
       scope: workSection
@@ -136,48 +151,103 @@ export function WorkSection() {
         </div>
         <div
           id="work-intro-section-work-word"
-          className="text-[24rem] font-bold"
+          className="text-9xl font-bold sm:text-[12rem] md:text-[16rem] lg:text-[24rem]"
         >
           Work
         </div>
       </div>
 
       <div className="absolute inset-0 h-full w-full">
-        <div
-          id="work-lighlight-container"
-          className="flex h-full w-full flex-col items-center justify-center gap-4"
-        >
-          <div id="project-counting-info" className="flex w-4/6 uppercase">
-            <div className="text-2xl font-extralight text-muted-foreground">
-              1
+        <div className="relative h-full w-full">
+          <div
+            id="work-lighlight-container"
+            className="flex h-full w-full flex-col items-center justify-center gap-4"
+          >
+            <div
+              id="project-counting-info"
+              className="flex w-5/6 uppercase md:w-4/6"
+            >
+              <div className="text-2xl font-extralight text-muted-foreground">
+                1
+              </div>
+              <div className="ml-auto text-sm text-muted-foreground">/3</div>
             </div>
-            <div className="ml-auto text-sm text-muted-foreground">/3</div>
-          </div>
-          <div className="relative aspect-video w-4/6">
-            <Image
-              src={
-                "https://images.unsplash.com/photo-1483366774565-c783b9f70e2c?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              }
-              alt="haha"
-              fill
-              className="overflow-clip rounded-lg"
-            />
+            <div className="relative aspect-video w-5/6 md:w-4/6">
+              <Image
+                src={
+                  "https://images.unsplash.com/photo-1483366774565-c783b9f70e2c?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                }
+                alt="haha"
+                fill
+                className="overflow-clip rounded-lg"
+              />
+            </div>
+
+            <div
+              id="project-description"
+              className="flex w-5/6 flex-col uppercase md:w-4/6 md:flex-row"
+            >
+              <h3
+                id="project-description-title"
+                className="h-fit text-3xl font-light"
+              >
+                <span className="inline-block">360 virtual tour</span>
+              </h3>
+              <p
+                id="project-description-paragraph"
+                className="max-w-xs text-sm normal-case md:ml-auto"
+              >
+                Enabling visiting future home online with 360 degree technology
+                right at the comfort of your web browser
+              </p>
+            </div>
           </div>
 
-          <div id="project-description" className="flex w-4/6 uppercase">
-            <h3
-              id="project-description-title"
-              className="h-fit text-3xl font-light"
+          <div
+            id="work-lighlight-container-2"
+            className="absolute inset-0 flex h-full w-full flex-col items-center justify-center gap-4"
+          >
+            <div
+              id="project-counting-info-2"
+              className="flex w-5/6 uppercase opacity-0 md:w-4/6"
             >
-              <span className="inline-block">360 virtual tour</span>
-            </h3>
-            <p
-              id="project-description-paragraph"
-              className="ml-auto max-w-xs text-sm normal-case"
+              <div className="text-2xl font-extralight text-muted-foreground">
+                1
+              </div>
+              <div className="ml-auto text-sm text-muted-foreground">/3</div>
+            </div>
+            <div
+              id="work-lighlight-figure-2"
+              className="relative aspect-video w-5/6 md:w-4/6"
             >
-              Enabling visiting future home online with 360 degree technology
-              right at the comfort of your web browser
-            </p>
+              <Image
+                src={
+                  "https://images.unsplash.com/photo-1531591022136-eb8b0da1e6d0?q=80&w=1412&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                }
+                alt="haha"
+                fill
+                className="overflow-clip rounded-lg"
+              />
+            </div>
+
+            <div
+              id="project-description-2"
+              className="flex w-5/6 flex-col uppercase opacity-0 md:w-4/6 md:flex-row"
+            >
+              <h3
+                id="project-description-title-2"
+                className="h-fit text-3xl font-light"
+              >
+                <span className="inline-block">360 virtual tour</span>
+              </h3>
+              <p
+                id="project-description-paragraph-2"
+                className="max-w-xs text-sm normal-case md:ml-auto"
+              >
+                Enabling visiting future home online with 360 degree technology
+                right at the comfort of your web browser
+              </p>
+            </div>
           </div>
         </div>
       </div>
