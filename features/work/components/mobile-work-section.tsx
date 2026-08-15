4f89943215a1f989555b87e928c7c3acc0cd7530
Sokhuong <sokhuong.usk@gmail.com>
2026-08-15 19:21:54 +0700

    feat: Add animation for titles in mobile work section

--- features/work/components/mobile-work-section.tsx
+++ features/work/components/mobile-work-section.tsx
@@ -1,16 +1,75 @@
 import Image from "next/image"
-import { ComponentProps } from "react"
+import { ComponentProps, useRef } from "react"
 
+import { useGSAP } from "@gsap/react"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+
+import { gsap } from "@/lib/gsap"
 import { cn } from "@/lib/utils"
 
 import { works } from "./work-section"
+
+gsap.registerPlugin(useGSAP, ScrollTrigger)
 
 export function MobileWorkSection({
   className,
   ...props
 }: ComponentProps<"div">) {
+  const mobileWorkSectionRef = useRef<HTMLDivElement>(null)
+
+  useGSAP(
+    () => {
+      const textWrappers = document.querySelectorAll("[data-info=text-wrapper]")
+      gsap.set("[data-info=text-wrapper] > h3", {
+        clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 100%)"
+      })
+      gsap.set("[data-info=text-wrapper] > h3 > span", {
+        yPercent: 100
+      })
+
+      textWrappers.forEach((wrapper) => {
+        const header = wrapper.querySelector("h3")
+        const description = wrapper.querySelector("p")
+        if (!header || !description) return
+
+        ScrollTrigger.create({
+          trigger: wrapper,
+          start: "top bottom",
+          animation: gsap
+            .timeline()
+            .fromTo(
+              header.querySelector("span"),
+              {
+                yPercent: 100
+              },
+              {
+                yPercent: 0
+              }
+            )
+            .fromTo(
+              header,
+              {
+                clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 100%)"
+              },
+              {
+                clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"
+              },
+              0
+            )
+        })
+      })
+    },
+    {
+      scope: mobileWorkSectionRef,
+      dependencies: []
+    }
+  )
   return (
-    <section className={cn("relative text-foreground", className)} {...props}>
+    <section
+      ref={mobileWorkSectionRef}
+      className={cn("relative text-foreground", className)}
+      {...props}
+    >
       <h2 className="mb-20 text-center text-4xl font-bold text-muted-foreground uppercase">
         Work
       </h2>
@@ -27,8 +86,12 @@ export function MobileWorkSection({
                 alt="empty for now"
               />
             </div>
-            <h3 className="text-xl font-light uppercase">{work.title}</h3>
-            <p className="text-muted-foreground">{work.description}</p>
+            <div data-info="text-wrapper" className="space-y-2">
+              <h3 className="w-fit text-xl font-light uppercase">
+                <span className="inline-block">{work.title}</span>
+              </h3>
+              <p className="text-muted-foreground">{work.description}</p>
+            </div>
           </li>
         ))}
       </ul>
