4c668f580e6ba988e8807f47b47b7a46e4d2aabb
Sokhuong <sokhuong.usk@gmail.com>
2026-08-21 14:55:26 +0700

    chore(theme): Set default theme to dark

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -9,7 +9,7 @@ function ThemeProvider({
   return (
     <NextThemesProvider
       attribute="class"
-      defaultTheme="system"
+      defaultTheme="dark"
       enableSystem
       disableTransitionOnChange
       {...props}
